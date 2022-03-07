2ab899b0fd68012beb9355788398123578ddc945
wblanck <blanck.william@gmail.com>
2022-03-07 12:45:03 +0100

    navbar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { BsSearch, BsFillCartFill } from "react-icons/bs";
 import styled from "styled-components";
 
 const Container = styled.div`
@@ -8,30 +8,65 @@ const Wrapper = styled.div`
   padding: 10px 20px;
   display: flex;
   justify-content: space-between;
+  align-items: center;
 `;
 const Left = styled.div`
   flex: 1;
+  display: flex;
+  align-items: center;
 `;
 const Language = styled.span`
   font-size: 14px;
   cursor: pointer;
 `;
+const SearchContainer = styled.div`
+  border: 0.5px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+`;
+const Input = styled.input`
+  border: none;
+`;
 const Center = styled.div`
   flex: 1;
+  text-align: center;
+`;
+const Logo = styled.h1`
+  font-weight: bold;
 `;
 const Right = styled.div`
   flex: 1;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
 `;
-
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
+`;
 const Navbar = () => {
   return (
     <Container>
       <Wrapper>
         <Left>
           <Language>EN</Language>
+          <SearchContainer>
+            <Input />
+            <BsSearch style={{ color: "gray", fontSize: 16 }} />
+          </SearchContainer>
         </Left>
-        <Center>Center</Center>
-        <Right>right</Right>
+        <Center>
+          <Logo>Blanx.</Logo>
+        </Center>
+        <Right>
+          <MenuItem>Register</MenuItem>
+          <MenuItem>Sign In</MenuItem>
+          <MenuItem>
+            <BsFillCartFill />
+          </MenuItem>
+        </Right>
       </Wrapper>
     </Container>
   );
