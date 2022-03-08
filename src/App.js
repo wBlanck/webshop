319d436d52f917fb4e07593c44d909c600df10d1
wBlanck <blanck.william@gmail.com>
2022-03-08 12:32:16 +0100

    added product page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Product from "./pages/Product";
 import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
 
 function App() {
   return (
     <div>
-      <ProductList />
+      <Product />
     </div>
   );
 }
